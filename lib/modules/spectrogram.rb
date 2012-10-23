cb6c523d9414a195ac82b0242ccd1e052f686c60
Mark <qut.bioacoustics.research+mark@gmail.com>
2012-10-23 16:47:00 +1000

    Added audio console apps, spectrogram tests
    mp3splt and wavpack

--- lib/modules/spectrogram.rb
+++ lib/modules/spectrogram.rb
@@ -5,12 +5,19 @@ module Spectrogram
   @sox_arguments_sample_rate = "rate 22050"
   @sox_arguments_spectrogram = "spectrogram -m -r -l -a -q 249 -w hann -y 257 -X 43.06640625 -z 100"
   @sox_arguments_output = "-o"
-  @@my_logger ||= Logger.new("#{Rails.root}/log/my.log")
+  @my_logger ||= Logger.new("#{Rails.root}/log/my.log")
   def self.generate(source, destination)
+  
     # sox command to create a spectrogram from an audio file
     command = "#{@sox_path} #{@sox_arguments_verbose} \"#{source}\" #{@sox_arguments_output_audio} #{@sox_arguments_sample_rate} #{@sox_arguments_spectrogram} #{@sox_arguments_output} \"#{destination}\""
+    
     # run the command and wait for the result
     stdout_str, stderr_str, status = Open3.capture3(command)
+    
+    # log the command
+    @my_logger.debug(command)
+    
+    
     # package up all the available information and return it
     result = [ stdout_str, stderr_str, status, File.exist?(source), File.exist?(destination) ]
   end
