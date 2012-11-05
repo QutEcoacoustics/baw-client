dd1c24583f5cd542b2a116faf281e4695852bba1
Anthony Truskinger <anthony.truskinger@gmail.com>
2012-11-06 09:59:28 +1000

    configured html5 routing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,17 @@
 
 /* App Module */
 
-angular.module('baw', []).
-    config(['$routeProvider', function($routeProvider) {
+angular.module('baw', ['$routeProvider','$locationProvider'], function($routeProvider, $locationProvider ) {
+
+    // routes
     $routeProvider.
         when('/home', {templateUrl: 'assets/home.html',   controller: HomeCtrl}).
         when('/projects', {templateUrl: 'assets/project.html', controller: ProjectCtrl}).
         when('/site', {templateUrl: 'assets/site.html', controller: SiteCtrl}).
         //when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
         otherwise({redirectTo: '/home'});
-}]);
+
+    // location config
+    $locationProvider.html5Mode(true);
+
+});
