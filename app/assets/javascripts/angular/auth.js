b6153fde5065718421b02276f46e986dfa9ef2d8
Anthony Truskinger <anthony.truskinger@gmail.com>
2012-12-03 10:26:11 +1000

    small refactor

--- app/assets/javascripts/angular/auth.js
+++ app/assets/javascripts/angular/auth.js
@@ -31,24 +31,3 @@ angular.module('angular-auth', ['http-auth-interceptor', 'content-mocks'])
 
 
 
-function LoginCtrl($scope, $http, authService, PersonaAuthenticator) {
-
-
-    $scope.submit = function (provider) {
-
-        switch (provider) {
-            case "persona":
-                PersonaAuthenticator.login();
-                break;
-            default:
-                throw "Provider not matched";
-        }
-
-        console.info(result);
-
-        //$http.post(path).success(function () {
-        //    authService.loginConfirmed();
-        //});
-    }
-}
-LoginCtrl.$inject = ['$scope', '$http', 'authService', 'PersonaAuthenticator'];
