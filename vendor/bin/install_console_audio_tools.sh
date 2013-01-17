ee6aa19a9ce7ca1ed2e7accd6ed5681259794661
Mark <cofiem@gmail.com>
2013-01-17 23:02:47 +1000

    added apt-get upgrade

--- vendor/bin/install_console_audio_tools.sh
+++ vendor/bin/install_console_audio_tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing audio tools"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 16126D3A3E5C1192
-sudo apt-get update -q
+sudo apt-get update
+sudo apt-get upgrade
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 16126D3A3E5C1192
 sudo apt-get install ffmpeg mp3splt sox wavpack --fix-missing
-sudo apt-get update -q
