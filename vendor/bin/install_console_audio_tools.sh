2190778e9be9ed16f5bb792494e697aafbe764c9
Mark Cottman-Fields <qut.bioacoustics.research+mark@gmail.com>
2013-01-17 16:43:42 +1000

    Removed TestUnit test helpers and moved controller tests to rspec. Fixed problem in db seeds. changed init script for travis back to something more sane. Changed module tests to rspec.
    modified:   Gemfile.lock
    modified:   db/development_seeds.rb
    renamed:    test/functional/analysis_items_controller_test.rb -> spec/controllers/analysis_items_controller_spec.rb
    renamed:    test/functional/analysis_jobs_controller_test.rb -> spec/controllers/analysis_jobs_controller_spec.rb
    renamed:    test/functional/analysis_scripts_controller_test.rb -> spec/controllers/analysis_scripts_controller_spec.rb
    renamed:    test/functional/audio_events_controller_test.rb -> spec/controllers/audio_events_controller_spec.rb
    renamed:    test/functional/audio_recordings_controller_test.rb -> spec/controllers/audio_recordings_controller_spec.rb
    new file:   spec/controllers/bookmarks_controller_spec.rb
    new file:   spec/controllers/home_controller_spec.rb
    new file:   spec/controllers/permissions_controller_spec.rb
    new file:   spec/controllers/photos_controller_spec.rb
    new file:   spec/controllers/progresses_controller_spec.rb
    renamed:    test/functional/projects_controller_test.rb -> spec/controllers/projects_controller_spec.rb
    new file:   spec/controllers/saved_searches_controller_spec.rb
    new file:   spec/controllers/sites_controller_spec.rb
    new file:   spec/controllers/tags_controller_spec.rb
    new file:   spec/controllers/users_controller_spec.rb
    modified:   spec/models/authorization_spec.rb
    modified:   spec/models/bookmark_spec.rb
    modified:   spec/models/class_search_spec.rb
    modified:   spec/models/module_cache_spec.rb
    modified:   spec/models/module_harvester_spec.rb
    modified:   spec/models/module_spectrogram_spec.rb
    modified:   spec/models/permission_spec.rb
    modified:   spec/models/photo_spec.rb
    modified:   spec/models/progress_spec.rb
    modified:   spec/models/saved_search_spec.rb
    deleted:    test/functional/bookmarks_controller_test.rb
    deleted:    test/functional/home_controller_test.rb
    deleted:    test/functional/permissions_controller_test.rb
    deleted:    test/functional/photos_controller_test.rb
    deleted:    test/functional/progresses_controller_test.rb
    deleted:    test/functional/routes_test.rb
    deleted:    test/functional/saved_searches_controller_test.rb
    deleted:    test/functional/sites_controller_test.rb
    deleted:    test/functional/tags_controller_test.rb
    deleted:    test/functional/users_controller_test.rb
    deleted:    test/integration/.gitkeep
    deleted:    test/performance/browsing_test.rb
    deleted:    test/unit/.gitkeep
    deleted:    test/unit/helpers/analysis_items_helper_test.rb
    deleted:    test/unit/helpers/analysis_jobs_helper_test.rb
    deleted:    test/unit/helpers/analysis_scripts_helper_test.rb
    deleted:    test/unit/helpers/audio_events_helper_test.rb
    deleted:    test/unit/helpers/audio_recordings_helper_test.rb
    deleted:    test/unit/helpers/bookmarks_helper_test.rb
    deleted:    test/unit/helpers/home_helper_test.rb
    deleted:    test/unit/helpers/permissions_helper_test.rb
    deleted:    test/unit/helpers/photos_helper_test.rb
    deleted:    test/unit/helpers/progresses_helper_test.rb
    deleted:    test/unit/helpers/projects_helper_test.rb
    deleted:    test/unit/helpers/saved_searches_helper_test.rb
    deleted:    test/unit/helpers/sites_helper_test.rb
    deleted:    test/unit/helpers/tags_helper_test.rb
    deleted:    test/unit/helpers/users_helper_test.rb
    modified:   vendor/bin/install_console_audio_tools.sh

--- vendor/bin/install_console_audio_tools.sh
+++ vendor/bin/install_console_audio_tools.sh
@@ -1,15 +1,7 @@
-sudo apt-key del 16126D3A3E5C1192
-sudo apt-get update
-sudo apt-key finger
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
-sudo apt-get update
-sudo apt-get install ffmpeg mp3splt sox wavpack --fix-missing
-sudo apt-get update
+#!/usr/bin/env bash
 
-# http://askubuntu.com/a/86445 apt-get clean            # Remove cached packages
-cd /var/lib/apt
-mv lists lists.old       # Backup mirror info
-mkdir -p lists/partial   # Recreate directory structure
-apt-get clean
-apt-get update           # Fetch mirror info
-sudo apt-key finger
+echo "Installing audio tools"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 16126D3A3E5C1192
+sudo apt-get update -q
+sudo apt-get install ffmpeg mp3splt sox wavpack --fix-missing
+sudo apt-get update -q
