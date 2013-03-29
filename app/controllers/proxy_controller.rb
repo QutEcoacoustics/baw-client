34113d811672f8b4ba0e4ba2110aa9454cfd6f1c
Anthony Truskinger <qut.bioacoustics.research+anthony@gmail.com>
2013-03-29 22:39:03 +1000

    broke rapid scan

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -5,27 +5,35 @@ class ProxyController < ApplicationController
   skip_authorize_resource :only => [:default]
   skip_load_resource :only => [:default]
 
+
   def default
     http_proxy = BawSite::Application.config.custom_proxy
+
+    if params[:proxy]
+      http_proxy = params[:proxy]
+    end
+
+    puts "HTTP PROXY for site is #{http_proxy}!!!!!!!!!!!!!!!"
 
     conn = Faraday.new(:url => params[:url]) do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+
       faraday.proxy http_proxy
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+
     end
 
     response = conn.get
 
-    result = ""
-    result += response.status.to_s
+    result = http_proxy + "\n"
+    result += (response.status || '{empty status}').to_s
     result += "\n <pre>"
-    result += (response.headers).to_yaml
+    result += (response.headers).to_yaml || '{empty headers}'
     result += "\n </pre>"
     result += "\n <hr/>"
-    result += response.body
+    result += response.body || '{empty body}'
 
     render text: result
   end
-
 end
