fd1e641f6cfd811b7a60fff1039d1f34a98edd54
Anthony Truskinger <anthony.truskinger@gmail.com>
2013-11-13 20:57:23 +1000

    cleaned out old dependicies from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,12 +15,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-conventional-changelog');
     grunt.loadNpmTasks('grunt-bump');
-    grunt.loadNpmTasks('grunt-coffeelint');
-    /*grunt.loadNpmTasks('grunt-recess');*/
-    //    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-ngmin');
@@ -306,26 +302,6 @@ module.exports = function (grunt) {
         },
 
         /**
-         * `grunt coffee` compiles the CoffeeScript sources. To work well with the
-         * rest of the build, we have a separate compilation task for sources and
-         * specs so they can go to different places. For example, we need the
-         * sources to live with the rest of the copied JavaScript so we can include
-         * it in the final build, but we don't want to include our specs there.
-         */
-        coffee: {
-            source: {
-                options: {
-                    bare: true
-                },
-                expand: true,
-                cwd: '.',
-                src: [ '<%= app_files.coffee %>' ],
-                dest: '<%= build_dir %>',
-                ext: '.js'
-            }
-        },
-
-        /**
          * `ng-min` annotates the sources before minifying. That is, it allows us
          * to code without the array syntax.
          */
@@ -471,24 +447,6 @@ module.exports = function (grunt) {
                 force: true
             },
             globals: {}
-        },
-
-        /**
-         * `coffeelint` does the same as `jshint`, but for CoffeeScript.
-         * CoffeeScript is not the default in ngBoilerplate, so we're just using
-         * the defaults here.
-         */
-        coffeelint: {
-            src: {
-                files: {
-                    src: [ '<%= app_files.coffee %>' ]
-                }
-            },
-            test: {
-                files: {
-                    src: [ '<%= app_files.coffeeunit %>' ]
-                }
-            }
         },
 
         /**
@@ -673,17 +631,6 @@ module.exports = function (grunt) {
             },
 
             /**
-             * When our CoffeeScript source files change, we want to run lint them and
-             * run our unit tests.
-             */
-            //coffeesrc: {
-            //    files: [
-            //        '<%= app_files.coffee %>'
-            //    ],
-            //    tasks: [ 'coffeelint:src', 'coffee:source', 'karma:unit:run', 'copy:build_appjs' ]
-            //},
-
-            /**
              * When assets are changed, copy them. Note that this will *not* copy new
              * files, so this is probably not very useful.
              */
@@ -737,20 +684,6 @@ module.exports = function (grunt) {
                 options: {
                     livereload: false
                 }
-            },
-
-            /**
-             * When a CoffeeScript unit test file changes, we only want to lint it and
-             * run the unit tests. We don't want to do any live reloading.
-             */
-            coffeeunit: {
-                files: [
-                    '<%= app_files.coffeeunit %>'
-                ],
-                tasks: [ 'coffeelint:test', 'karma:unit:run' ],
-                options: {
-                    livereload: false
-                }
             }
         }
 
@@ -779,7 +712,7 @@ module.exports = function (grunt) {
      * The `build` task gets your app ready to run for development and testing.
      */
     grunt.registerTask('build', [
-        'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', /*'recess:build',*/ 'sass:build',
+        'clean', 'html2js', 'jshint', 'sass:build',
         'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
         'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
         'karma:continuous'
