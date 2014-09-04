94807917ee18d78e71fbe3787809a100b09648b6
Anthony Truskinger <anthony.truskinger@gmail.com>
2014-09-04 16:01:31 +1000

    almost finished query builder - tests still failing

--- src/components/services/queryBuilder.js
+++ src/components/services/queryBuilder.js
@@ -1,74 +1,147 @@
 var qb = angular.module("bawApp.services.queryBuilder",  ["bawApp.configuration"]);
 
-function Query() {
+function Query(currentFieldKey) {
+    var currentField = currentFieldKey;
 
     this.filter = {};
-
-
 
     this.combinator = function combinator(type, functions) {
         if (!functions){
             return this;
         }
 
-        this.filter[type] = new Query();
+        // create a new level of nesting IFF there is already a root combinator
+        var that = this;
+        if((this instanceof RootQuery) && (Object.keys(this.filter).length >= 1)) {
+            that = new Query(this.getField());
+        }
 
-        var that = this.filter[type];
+        that.filter[type] = {};
+
         functions.forEach(function(value, key) {
             if (!(value instanceof Query)){
                 throw "A combinator only accepts Query objects";
             }
 
-            that.filter[key] = value.filter;
+            Query.deepMergeFilter(that.filter[type], value.filter);
         });
 
         return this;
     };
 
-    this.operator = function operator(operation, field, value) {
-        var that = this;
+    this.operator = function operator(operation, value, field) {
+        var that = this, operatorField = field;
         if (this instanceof RootQuery) {
-            that = new Query();
+            that = new Query(this.getField());
         }
 
-        that.filter[field] = that.filter[field] || {};
+        if (operatorField) {
+            that.field(operatorField);
+        }
+        else {
+            operatorField = that.getField();
+        }
 
-        that.filter[field][operation] = value;
+        if (!operatorField) {
+            throw "A field is not set - using an operator does not make sense";
+        }
+
+        that.filter[operatorField] = that.filter[operatorField] || {};
+        that.filter[operatorField][operation] = value;
 
         return that;
     };
 
+    this.field = function field(fieldKey) {
+        currentField = fieldKey;
+        return this;
+    };
+
+    this.getField = function getField() {
+        return currentField;
+    };
 }
 
-Query.mergeFilter = function mergeFilter(rootQuery, childQuery) {
-    if (rootQuery instanceof Query && childQuery instanceof Query) {
+Query.deepMergeFilter = function mergeFilter(base, child) {
+    Object.keys(child).forEach(function (key, index) {
+        if (angular.isObject(base[key])) {
+            Query.deepMergeFilter(base[key], child[key]);
+        }
+        else {
+            base[key] = child[key];
+        }
+    });
+};
 
-    }
-    else {
-        throw "Query objects not passed in";
-    }
+var validCombinators = {
+    "and": undefined,
+    "or": undefined,
+    "not": function(args) {
+        if (args.length > 1) {
+            throw "Not combinator only accepts one argument (either combinator or operator)";
+        }
+    }};
+
+var validOperators = {
+    "equal": undefined,
+    "eq": undefined,
+    "notEqual": undefined,
+    "notEq": undefined,
+    "lessThan": undefined,
+    "lt": undefined,
+    "lessThanOrEqual": undefined,
+    "lteq": undefined,
+    "greaterThan": undefined,
+    "gt": undefined,
+    "greaterThanOrEqual": undefined,
+    "gteq": undefined,
+    "range": undefined,
+    "in": function(field, value) {
+        if (!angular.isArray(value)) {
+            throw "The in function must be given as an array";
+        }
+    },
+    "contains": undefined,
+    "startsWith": undefined,
+    "endsWith": undefined
 };
 
 
-Query.prototype.eq = function eq(field, value) {
-    return this.operator("eq", field, value);
-};
+Object.keys(validCombinators).forEach(function (combinatorKey) {
+    Query.prototype[combinatorKey] = function() {
+        var args = Array.prototype.slice.call(arguments, 0);
 
-Query.prototype.and = function and() {
-    var args = Array.prototype.slice.call(arguments, 0);
-    return this.combinator("and", args);
-};
+        var validator = validCombinators[combinatorKey];
+        if (validator) {
+            validator(arguments);
+        }
 
-Query.prototype.or = function or() {
-    var args = Array.prototype.slice.call(arguments, 0);
-    return this.combinator("or", args);
-};
+        return this.combinator(combinatorKey, args);
+    };
+});
+
+Object.keys(validOperators).forEach(function(operatorKey) {
+    Query.prototype[operatorKey] = function(field, value) {
+        if (arguments.length == 1) {
+            value = field;
+            field = undefined;
+        }
+
+
+        var validator = validOperators[operatorKey];
+        if (validator) {
+            validator(field, value);
+        }
+
+        return this.operator(operatorKey, value, field);
+    };
+});
 
 
 function RootQuery() {
     Query.call(this);
 
-    this.sort = {};
+    this.sorting = {};
     this.paging = {};
 
     this.toJSON = function toJSON(spaces) {
--- src/components/services/queryBuilder.spec.js
+++ src/components/services/queryBuilder.spec.js
@@ -2,9 +2,21 @@ describe("The QueryBuilder", function () {
 
     var queryBuilder, q;
 
+    var validCombinators = ["and", "or", "not"];
+    var validOperators = [
+        "equal", "eq", "notEqual", "notEq",
+        "lessThan", "lt", "lessThanOrEqual", "lteq",
+        "greaterThan", "gt", "greaterThanOrEqual", "gteq",
+        "range", "in", "contains", "startsWith", "endsWith"
+    ];
+    var rootOperators = [
+        "paging", "sorting"
+    ];
+
     var spaces = 2;
+
     function j(obj) {
-        JSON.stringify(obj, null, spaces);
+        return JSON.stringify(obj, null, spaces);
     }
 
     beforeEach(module("bawApp.services.queryBuilder"));
@@ -16,25 +28,43 @@ describe("The QueryBuilder", function () {
     }]));
 
 
-    it("should be able to be created", function() {
+    it("should be able to be created", function () {
         var q = queryBuilder.create();
-
         expect(q instanceof RootQuery).toBeTrue();
     });
 
-    it("a query combinator should return itself", function() {
-       var actual = q.and(q.eq("field", 3.0));
+    it("should implement the expected interface", function () {
+        var queryInterface = validCombinators.concat(validOperators);
+        var rootInterface = queryInterface.concat(rootOperators);
+
+        var query = new Query();
+        var expected = {};
+        queryInterface.forEach(function (value) {
+            expected[value] = undefined;
+        });
+        expect(query).toImplement(expected);
+
+        var root = new RootQuery();
+        expected = {};
+        rootInterface.forEach(function (value) {
+            expected[value] = undefined;
+        });
+        expect(root).toImplement(expected);
+    });
+
+    it("a query combinator should return itself", function () {
+        var actual = q.and(q.eq("field", 3.0));
 
         expect(actual).toBe(q);
     });
 
-    it("will throw if a combinator is passed a non-query object", function() {
-        expect(function() {
+    it("will throw if a combinator is passed a non-query object", function () {
+        expect(function () {
             q.combinator("and", [{}]);
         }).toThrow("A combinator only accepts Query objects");
     });
 
-    it("a query operator should return a new instance of a Query", function() {
+    it("a query operator should return a new instance of a Query", function () {
         var actual = q.eq("field", 3.0);
 
         expect(actual instanceof Query).toBeTrue();
@@ -45,7 +75,18 @@ describe("The QueryBuilder", function () {
         expect(actual).not.toBe(q);
     });
 
-    it("should be able to do basic equality", function() {
+
+    it("should fail if a field is not set", function() {
+        expect(function() {
+            q.eq(3.0);
+        }).toThrow();
+    });
+
+    it("should allow a new field to be set", function() {
+
+    });
+
+    it("should be able to do basic equality", function () {
         var expected = {
             filter: {
                 and: {
@@ -58,17 +99,239 @@ describe("The QueryBuilder", function () {
 
         var actual = q.and(q.eq("field", 3.0));
 
+        expect(actual.toJSON(spaces)).toBe(j(expected));
+    });
+
+    it("should ensure not is arity:1 only", function () {
+        expect(function () {
+            q.not(q.field("fieldA").eq(3.0), q.field("fieldB").eq(4.0));
+        }).toThrow("Not combinator only accepts one argument (either combinator or operator)");
+    });
+
+    it("should allow one operation at root level", function() {
+        var expected = {
+            filter: {
+                field: {
+                    eq: 3.0
+                }
+            }
+        };
+
+        var actual = q.eq("field", 3.0);
 
         expect(actual.toJSON(spaces)).toBe(j(expected));
     });
 
+    it("should not allow more than one operation at root level", function() {
+
+        expect(function () {
+            var actual = q.eq("field", 3.0).eq("fieldB", 6.0);
+        }).toThrow();
+    });
+
+    it("should handle a more complex query", function () {
+        var expected = {
+            filter: {
+                and: {
+                    fieldA: {
+                        lt: 3.0,
+                        gt: 0.0
+                    },
+                    fieldB: {
+                        contains: "hello"
+                    }
+                }
+            }
+        };
+
+        var actual = q.and(
+            q.lt("fieldA", 3.0).contains("fieldB", "hello").gt("fieldA", 0.0)
+        );
+        expect(actual.toJSON(spaces)).toBe(j(expected));
+    });
+
+    it("should handle a more complex query - with separate queries for the same field", function () {
+        var expected = {
+            filter: {
+                and: {
+                    fieldA: {
+                        lt: 3.0,
+                        gt: 0.0
+                    },
+                    fieldB: {
+                        contains: "hello"
+                    }
+                }
+            }
+        };
+
+        var actual = q.and(
+            q.lt("fieldA", 3.0).contains("fieldB", "hello"),
+            q.gt("fieldA", 0.0)
+        );
+        expect(actual.toJSON(spaces)).toBe(j(expected));
+    });
+
+    it("should handle a more complex query - with lots of nesting", function () {
+        var expected = {
+            filter: {
+                and: {
+                    fieldA: {
+                        lt: 3.0,
+                        gt: 0.0
+                    },
+                    fieldB: {
+                        contains: "hello"
+                    },
+                    or: {
+                        and: {
+                            fieldA: {
+                                notEq: 6.0,
+                                lt: 20.0
+                            }
+                        },
+                        fieldB: {
+                            lt: 7.0
+                        },
+                        or: {
+                            fieldA:{
+                                lt: 20.0
+                            },
+                            fieldB: {
+                                gt: 4.0
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        var actual = q.and(
+            q.lt("fieldA", 3.0).contains("fieldB", "hello"),
+            q.gt("fieldA", 0.0),
+            q.or(
+                q.and(q.field("fieldA").notEq(6.0).lt(20.0)).lt("fieldB", 7.0),
+                q.or(
+                    q.lt("fieldA", 20.0),
+                    q.gt("fieldB", 4.0)
+                )
+            )
+        );
+        expect(actual.toJSON(spaces)).toBe(j(expected));
+    });
 
 
+    it("should handle a very complex query", function() {
+       var expected = {
+           "filter": {
+               "and": {
+                   "site_id": {
+                       "less_than": 123456,
+                       "greater_than": 9876,
+                       "in": [
+                           1,
+                           2,
+                           3
+                       ],
+                       "range": {
+                           "from": 100,
+                           "to": 200
+                       }
+                   },
+                   "status": {
+                       "greater_than_or_equal": 4567,
+                       "contains": "contain text",
+                       "starts_with": "starts with text",
+                       "ends_with": "ends with text",
+                       "range": {
+                           "interval": "[123, 128]"
+                       }
+                   },
+                   "or": {
+                       "duration_seconds": {
+                           "not_eq": 40
+                       },
+                       "not": {
+                           "channels": {
+                               "less_than_or_equal": 9999
+                           }
+                       }
+                   }
+               },
+               "or": {
+                   "recorded_date": {
+                       "contains": "Hello"
+                   },
+                   "media_type": {
+                       "ends_with": "world"
+                   },
+                   "duration_seconds": {
+                       "eq": 60,
+                       "lteq": 70,
+                       "equal": 50,
+                       "gteq": 80
+                   },
+                   "channels": {
+                       "eq": 1,
+                       "less_than_or_equal": 8888
+                   }
+               },
+               "not": {
+                   "duration_seconds": {
+                       "not_eq": 140
+                   }
+               }
+           }/*,
+           "projection": {
+               "include": [
+                   "recorded_date",
+                   "site_id",
+                   "duration_seconds",
+                   "media_type"
+               ]
+           },
+           "sort": {
+               "order_by": "duration_seconds",
+               "direction": "desc"
+           },
+           "paging": {
+               "page": 1,
+               "items": 10
+           }*/
+       };
 
-//    var actual = q.and(
-//        q.eq("fieldA", 3.0),
-//        q.or(
-//            q.lt("fieldA", 30.0).ge("fieldB", 0.0)
-//        )
-//    );
+        var actual = q
+            .and(
+            q
+                .lessThan("siteId", 123456)
+                .greaterThan(9876)
+                .in([1,2,3])
+                .range({from: 100, to: 200}),
+            q.field("status")
+                .greaterThanOrEqual(4567)
+                .contains("contain text")
+                .startsWith("starts with text")
+                .endsWith("ends with text")
+                .range({interval: "[123, 128]"}),
+            q.or(
+                q.notEq("durationSeconds", 40),
+                q.not (
+                    q.lessThanOrEqual("channels", 9999)
+                )
+            )
+        ).or(
+            q.contains("recordedDate", "Hello"),
+            q.endsWith("mediaType", "world"),
+            q.field("durationSeconds")
+                .eq(60)
+                .lteq(70)
+                .equal(50)
+                .gteq(80),
+            q.eq("channels", 1).lessThanOrEqual(8888)
+        ).not(
+            q.notEq("durationSeconds", 140)
+        );
+
+        expect(actual.toJSON(spaces)).toBe(j(expected));
+    });
 });
