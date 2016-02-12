25926ef195020a32be5f3ce83e85894c0ba27f37
Anthony Truskinger <anthony.truskinger@gmail.com>
2016-02-12 23:30:45 +1000

    fix(browser): Fixed bowser integration
    API changed on recent major version bump

--- src/app/app.js
+++ src/app/app.js
@@ -452,9 +452,9 @@ angular.module("baw",
                 // only do it once - we best not be too annoying
                 var supported = constants.browserSupport;
                 var isSupportedBrowser = false;
-                var version = parseFloat(bowser.browser.version);
+                var version = parseFloat(bowser.version);
                 angular.forEach(supported.optimum, function (value, key) {
-                    if (isSupportedBrowser || (bowser.browser[key] && version >= value)) {
+                    if (isSupportedBrowser || (bowser[key] && version >= value)) {
                         isSupportedBrowser = true;
                     }
                 });
@@ -465,11 +465,11 @@ angular.module("baw",
 
                             var supportedBrowser = false;
                             angular.forEach(supported.supported, function (value, key) {
-                                if (bowser.browser[key]) {
+                                if (bowser[key]) {
                                     if (version >= value) {
                                         growl.info(supported.baseMessage.format({
-                                            name: bowser.browser.name,
-                                            version: bowser.browser.version,
+                                            name: bowser.name,
+                                            version: bowser.version,
                                             reason: "not well tested"
                                         }));
                                         supportedBrowser = true;
@@ -483,8 +483,8 @@ angular.module("baw",
 
                             if (!supportedBrowser) {
                                 growl.warning(supported.baseMessage.format({
-                                    name: bowser.browser.name,
-                                    version: bowser.browser.version,
+                                    name: bowser.name,
+                                    version: bowser.version,
                                     reason: "not supported"
                                 }));
                             }
