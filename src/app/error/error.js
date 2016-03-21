f38391caffaffbf65d97c3564886387063248502
Anthony Truskinger <anthony.truskinger@gmail.com>
2016-03-21 10:16:55 +1000

    fix(layout): fix up various visual assets
    Also fixes full page layout bug

--- src/app/error/error.js
+++ src/app/error/error.js
@@ -1,20 +1,7 @@
-//angular.module('home', []).config(function ($routeProvider, $httpProvider) {
-//
-//    $routeProvider.
-//        when('/', {templateUrl: '/assets/home.html',   controller: HomeCtrl}).
-//        otherwise({redirectTo: '/'});
-//
-//    //$httpProvider.defaults.headers.
-//      //  common['X-CSRF-Token'] = $['meta[name=csrf-token]'].attr('content');
-//});
 angular.module("bawApp.error", [])
-    .controller("ErrorCtrl", ["$scope",
-
-
-        function ErrorCtrl($scope) {
-
+    .controller("ErrorController", [
+        "$scope",
+        function ErrorController($scope) {
             $scope.message = "We can't seem to find what you are looking for (404)";
-
-
         }
     ]);
