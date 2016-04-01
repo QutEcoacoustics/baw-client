f6efe482b9d7beb839d1d9d106dea55f61e420b1
Anthony Truskinger <anthony.truskinger@gmail.com>
2016-04-01 14:00:07 +1000

    feat(pagination): Introduce pagination-href directive
    Pagination href-directive allows us to execute an arbitrary function for
    generating pagination hyperlinks.

--- src/components/directives/angular-ui/bootstrap/pagination/pagination.js
+++ src/components/directives/angular-ui/bootstrap/pagination/pagination.js
@@ -1,6 +1,6 @@
 angular.module(
     "bawApp.directives.ui.bootstrap.pagination",
-    [])
+    ["ui.bootstrap.pagination"])
     .run([
         "$templateCache",
         function($templateCache) {
@@ -8,13 +8,41 @@ angular.module(
             const
                 targetTemplate = "uib/template/pagination/pagination.html",
                 pageRegex = /(href).*(?:ng-click="selectPage\(([^,]+), \$event\)")/gm,
-                replaceString = `ng-href="{{ $parent.$parent.getPaginationLink($2) }}" href`;
+                replaceString = `ng-href="{{ pagination.href($2) }}" href`;
 
             var oldTemplate = $templateCache.get(targetTemplate);
 
             var newTemplate = oldTemplate.replace(pageRegex, replaceString);
 
             $templateCache.put(targetTemplate, newTemplate);
-        }]);
+        }])
+    .directive("paginationHref", ["$parse", function($parse) {
+        return {
+            require: ["uibPagination"],
+            controller: "UibPaginationController",
+            controllerAs: "pagination",
+            replace: true,
+            link: function(scope, element, attrs, ctrls) {
+                var paginationCtrl = ctrls[0];
+                let parentScope = scope;
+
+                // this is dodgy AF but its the only way i can think of to get
+                // the instance for which the actual expression is attached!
+                let parts = attrs.paginationHref.split(".");
+                let parent = parentScope;
+                if (parts.length > 1) {
+                    parts.splice(-1, 1);
+                    let parentExpression = parts.join(".");
+                    parent = $parse(parentExpression)(parentScope);
+                }
+
+                let f = $parse(attrs.paginationHref)(parentScope);
+                paginationCtrl.href = function (...args) {
+                    return f.apply(parent, args);
+                };
+            }
+        };
+    }]);
+
 
 
