9c937173f0b5dd4c2bf2fcaa4660cfd391506b54
Phil <phil.eichinski@gmail.com>
2016-09-21 15:54:25 +1000

    position line uses unitConverter

--- src/app/annotationViewer/positionLine.js
+++ src/app/annotationViewer/positionLine.js
@@ -3,7 +3,7 @@ var bawds = bawds || angular.module("bawApp.directives", [
         "bawApp.directives.ui.bootstrap",
         "bawApp.directives.formChildrenHack"]);
 
-bawds.directive("positionLine", function () {
+bawds.directive("positionLine", ["bawApp.unitConverter", function (unitConverter) {
 
     return {
         restrict: "A",
@@ -11,9 +11,30 @@ bawds.directive("positionLine", function () {
         replace: true,
         scope: {
             media: "<media",
-            audioData: "<audioData"
+            audioData: "<audioData",
+            imageSelector: "<imageId"
         },
         link: function (scope, elements, attributes, controller) {
+
+            scope.converters = false;
+
+            scope.$watch(function () { return scope.media; }, function (newValue, oldValue) {
+
+                if (newValue.sampleRate) {
+                    scope.converters = unitConverter.getConversions({
+                        sampleRate: newValue.sampleRate,
+                        spectrogramWindowSize: newValue.spectrogram ? newValue.spectrogram.windowSize : null,
+                        endOffset: newValue.endOffset,
+                        startOffset: newValue.startOffset,
+                        imageElement: angular.element( document.querySelector( scope.imageSelector ) ),
+                        audioRecordingAbsoluteStartDate: newValue.recordedDate
+                    });
+                }
+
+            });
+
+
+
 
             scope.positionLine = function () {
 
@@ -22,13 +43,20 @@ bawds.directive("positionLine", function () {
                     return 0;
                 }
 
+                if (scope.converters) {
+
+                    console.log("ready to convert stuff!");
+                    return(scope.converters.secondsToPixels(this.audioData.position));
+
+                }
+
                 // todo: use unit converter
-                var durationSeconds = (this.media.commonParameters.endOffset - this.media.commonParameters.startOffset);
-                var fractionCompleted = this.audioData.position / durationSeconds;
-                fractionCompleted = (fractionCompleted > 1) ? 1 : fractionCompleted;
-                var totalPixels = durationSeconds * this.media.spectrogram.ppms * 1000;
-                var pixelOffset = totalPixels * fractionCompleted;
-                return Math.round(pixelOffset);
+                // var durationSeconds = (this.media.commonParameters.endOffset - this.media.commonParameters.startOffset);
+                // var fractionCompleted = this.audioData.position / durationSeconds;
+                // fractionCompleted = (fractionCompleted > 1) ? 1 : fractionCompleted;
+                // var totalPixels = durationSeconds * this.media.spectrogram.ppms * 1000;
+                // var pixelOffset = totalPixels * fractionCompleted;
+                // return Math.round(pixelOffset);
 
             };
 
@@ -36,4 +64,4 @@ bawds.directive("positionLine", function () {
     };
 
 
-});
+}]);
