6742a729d785389f00c2f99c9d8d536297f32f50
Phil <phil.eichinski@gmail.com>
2016-09-21 14:57:45 +1000

    isolated scope for position line

--- src/app/annotationViewer/positionLine.js
+++ src/app/annotationViewer/positionLine.js
@@ -5,27 +5,32 @@ var bawds = bawds || angular.module("bawApp.directives", [
 
 bawds.directive("positionLine", function () {
 
-
     return {
         restrict: "A",
         templateUrl: "annotationViewer/positionLine.tpl.html",
         replace: true,
+        scope: {
+            media: "<media",
+            audioData: "<audioData"
+        },
         link: function (scope, elements, attributes, controller) {
 
             scope.positionLine = function () {
-                if (!this.model.audioElement || !this.media) {
+
+                if (!this.audioData || !this.media) {
+                    console.log("no audio data or no media");
                     return 0;
                 }
 
+                // todo: use unit converter
                 var durationSeconds = (this.media.commonParameters.endOffset - this.media.commonParameters.startOffset);
-                var fractionCompleted = this.model.audioElement.position / durationSeconds;
+                var fractionCompleted = this.audioData.position / durationSeconds;
                 fractionCompleted = (fractionCompleted > 1) ? 1 : fractionCompleted;
                 var totalPixels = durationSeconds * this.media.spectrogram.ppms * 1000;
                 var pixelOffset = totalPixels * fractionCompleted;
                 return Math.round(pixelOffset);
 
             };
-
 
         }
     };
