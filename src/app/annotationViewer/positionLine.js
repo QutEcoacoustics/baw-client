a9d263352f9b6ea1e7703e4052442ede1bd4b185
Phil <phil.eichinski@gmail.com>
2016-11-29 17:45:17 +1000

    fix filenames and syntax errors that somehow were introduced by merge

--- src/app/annotationViewer/positionLine.js
+++ src/app/annotationViewer/positionLine.js
@@ -1,9 +1,6 @@
-var bawds = bawds || angular.module("bawApp.directives", [
-        "bawApp.configuration",
-        "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"]);
-
-bawds.directive("positionLine", ["bawApp.unitConverter", "$window", function (unitConverter, $window) {
+angular
+    .module("bawApp.annotationViewer.positionLine", [])
+    .directive("positionLine", ["bawApp.unitConverter", "$window", function (unitConverter, $window) {
 
     return {
         restrict: "A",
@@ -19,54 +16,6 @@ bawds.directive("positionLine", ["bawApp.unitConverter", "$window", function (un
             scope.converters = false;
 
             var image = document.querySelector("#"+scope.imageId);
-            //var image = angular.element("#"+scope.imageId);
-
-            // scope.$watch(function () { return scope.media; }, function (newValue, oldValue) {
-            //         scope.updateConverters(newValue);
-            // });
-
-            // scope.$watch("image.clientWidth", function () {
-            //     scope.updateConverters();
-            // });
-            //
-            //
-            // scope.updateConverters = function (media) {
-            //
-            //     if (!media || !media.startOffset) {
-            //         return;
-            //     }
-            //
-            //     // scope.converters = unitConverter.getConversions({
-            //     //     sampleRate: media.sampleRate,
-            //     //     spectrogramWindowSize: media.spectrogram ? media.spectrogram.windowSize : null,
-            //     //     endOffset: media.endOffset,
-            //     //     startOffset: media.startOffset,
-            //     //     imageElement: image,
-            //     //     audioRecordingAbsoluteStartDate: media.recordedDate
-            //     // });
-            //
-            //     var data = {
-            //         endOffset: media.endOffset,
-            //         startOffset: media.startOffset,
-            //         imageElement: image
-            //     };
-            //
-            //     scope.converters = (function (data) {
-            //         var duration = data.endOffset - data.startOffset;
-            //         var imageWidth = data.imageElement.clientWidth;
-            //         return {
-            //             secondsToPixels: function (audioPositionSeconds) {
-            //
-            //                 var pixelPosition = imageWidth * (audioPositionSeconds / duration);
-            //                 return pixelPosition;
-            //
-            //             }
-            //         };
-            //     }(data));
-            //
-            //     console.log("converters", scope.converters);
-            //
-            // };
 
             scope.secondsToPixels = function (audioPositionSeconds) {
                 var pixelPosition, imageWidth;
@@ -89,14 +38,6 @@ bawds.directive("positionLine", ["bawApp.unitConverter", "$window", function (un
             };
 
 
-            // scope.getImageWidth = function () {
-            //     return image.clientWidth;
-            // };
-            //
-            // scope.$watch(scope.getElementDimensions, function () {
-            //     scope.updateConverters(scope.media);
-            // }, true);
-
             angular.element($window).on("resize", function () {
                 //scope.updateConverters(scope.media);
                 scope.$apply();
@@ -105,15 +46,7 @@ bawds.directive("positionLine", ["bawApp.unitConverter", "$window", function (un
             scope.positionLine = function () {
                 return(scope.secondsToPixels(this.audioData.position));
 
-                // if (image.clientWidth === 0) {
-                //     scope.updateConverters(scope.media);
-                // }
 
-                // if (scope.converters) {
-                //     return(scope.secondsToPixels(this.audioData.position));
-                // } else {
-                //     return 0;
-                // }
 
             };
 
--- src/components/models/associations.js
+++ src/components/models/associations.js
@@ -91,155 +91,154 @@ angular
             }
         ]
     ).factory(
-        "baw.models.ApiBase",
-        [
-            "baw.models.Capabilities",
-            function (Capabilities) {
-                let metaKey = Symbol("meta");
+    "baw.models.ApiBase",
+    [
+        "baw.models.Capabilities",
+        function (Capabilities) {
+            let metaKey = Symbol("meta");
 
-                class ApiBase {
+            class ApiBase {
 
-                    constructor(resource) {
-                        Object.assign(this, resource);
+                constructor(resource) {
+                    Object.assign(this, resource);
 
-                        // createdAt and UpdatedAt are fairly common attributes
-                        if (this.createdAt) {
-                            this.createdAt = new Date(this.createdAt);
-                        }
-
-                        if (this.updatedAt) {
-                            this.updatedAt = new Date(this.updatedAt);
-                        }
-
-                        if (this.creatorId) {
-                            this.creatorId = Number(this.creatorId);
-                        }
-
-                        if (this.updaterId) {
-                            this.updaterId = Number(this.updaterId);
-                        }
+                    // createdAt and UpdatedAt are fairly common attributes
+                    if (this.createdAt) {
+                        this.createdAt = new Date(this.createdAt);
                     }
 
-                    /**
-                     * If called, auto downloads linked resources.
-                     * Since we want customizable behaviour, we force an explicit call
-                     * to autoDownload.
-                     * @param resources
-                     */
-                    autoDownload(resources) {
-
+                    if (this.updatedAt) {
+                        this.updatedAt = new Date(this.updatedAt);
                     }
 
-                    get hasId() {
-                        return !(this.id === undefined || this.id === null);
+                    if (this.creatorId) {
+                        this.creatorId = Number(this.creatorId);
                     }
 
-                    /**
-                     * Holds the meta object from an API response.
-                     * @returns {*}
-                     */
-                    get meta() {
-                        return this[metaKey];
+                    if (this.updaterId) {
+                        this.updaterId = Number(this.updaterId);
                     }
+                }
 
-                    /**
-                     * Capability tester. Can an action be implemented?
-                     */
-                    can(actionName, reason) {
-                        return this[metaKey].capabilities.can(this, actionName, reason);
-                    }
+                /**
+                 * If called, auto downloads linked resources.
+                 * Since we want customizable behaviour, we force an explicit call
+                 * to autoDownload.
+                 * @param resources
+                 */
+                autoDownload(resources) {
 
-                    // security related helpers
-                    // note: ideally these should not be needed once capabilities are developed for the REST API
-                    isOwnedBy(user) {
-                        if (!user) {
-                            return null;
-                        }
+                }
 
-                        if (user.isAdmin) {
-                            return true;
-                        }
+                get hasId() {
+                    return !(this.id === undefined || this.id === null);
+                }
 
-                        if (this.creatorId) {
-                            return this.creatorId === user.id || this.updaterId === user.id;
-                        }
+                /**
+                 * Holds the meta object from an API response.
+                 * @returns {*}
+                 */
+                get meta() {
+                    return this[metaKey];
+                }
 
+                /**
+                 * Capability tester. Can an action be implemented?
+                 */
+                can(actionName, reason) {
+                    return this[metaKey].capabilities.can(this, actionName, reason);
+                }
+
+                // security related helpers
+                // note: ideally these should not be needed once capabilities are developed for the REST API
+                isOwnedBy(user) {
+                    if (!user) {
                         return null;
                     }
 
-
-
-                    static make(resource) {
-                        //noinspection JSValidateTypes
-                        if (this === window) {
-                            throw new Error("This static method must be called with a bound this");
-                        }
-
-                        return new this(resource);
+                    if (user.isAdmin) {
+                        return true;
                     }
 
-                    /**
-                     * This function generates a converter for models to use.constructor
-                     * This provided models with a function to convert API responses
-                     * into DS objects.
-                     *
-                     * TODO: remove the need to return the FULL APi object
-                     * @returns {}
-                     * @param response
-                     */
-                    static makeFromApi(response) {
-                        //noinspection JSValidateTypes
-                        if (this === window) {
-                            throw new Error("This static method must be called with a bound this");
-                        }
-
-                        var items = [];
-                        if (angular.isArray(response.data.data)) {
-                            //noinspection JSUnresolvedVariable
-                            items = response.data.data.map(this.make, this);
-                        } else {
-                            //noinspection JSUnresolvedFunction
-                            items[0] = this.make(response.data.data);
-                        }
-
-                        // allow all object to get and set their meta data object
-                        let meta = response.data.meta;
-
-                        // setup capabilities
-
-                        // HACK: while capabilities not supported by API we instead draw them from the client Models
-                        if (meta.capabilities === undefined) {
-                            if (this.capabilities) {
-                                console.warn(`Capabilities for ${this.name} are drawn from client model`);
-                                meta.capabilities = this.capabilities;
-                            }
-                            else {
-                                meta.capabilities = {};
-                            }
-                        }
-                        meta.capabilities = new Capabilities(meta.capabilities);
-
-                        // give each object a reference to meta
-                        items.forEach((a) => a[metaKey] = meta);
-
-                        response.data.data = items;
-
-                        return response;
+                    if (this.creatorId) {
+                        return this.creatorId === user.id || this.updaterId === user.id;
                     }
 
-                    static makeFromApiWithType(Type) {
-                        return function (resource) {
-                            return ApiBase.makeFromApi.call(Type, resource);
-                        };
-                    }
+                    return null;
                 }
 
 
+
+                static make(resource) {
+                    //noinspection JSValidateTypes
+                    if (this === window) {
+                        throw new Error("This static method must be called with a bound this");
+                    }
+
+                    return new this(resource);
                 }
 
-                return ApiBase;
+                /**
+                 * This function generates a converter for models to use.constructor
+                 * This provided models with a function to convert API responses
+                 * into DS objects.
+                 *
+                 * TODO: remove the need to return the FULL APi object
+                 * @returns {}
+                 * @param response
+                 */
+                static makeFromApi(response) {
+                    //noinspection JSValidateTypes
+                    if (this === window) {
+                        throw new Error("This static method must be called with a bound this");
+                    }
+
+                    var items = [];
+                    if (angular.isArray(response.data.data)) {
+                        //noinspection JSUnresolvedVariable
+                        items = response.data.data.map(this.make, this);
+                    } else {
+                        //noinspection JSUnresolvedFunction
+                        items[0] = this.make(response.data.data);
+                    }
+
+                    // allow all object to get and set their meta data object
+                    let meta = response.data.meta;
+
+                    // setup capabilities
+
+                    // HACK: while capabilities not supported by API we instead draw them from the client Models
+                    if (meta.capabilities === undefined) {
+                        if (this.capabilities) {
+                            console.warn(`Capabilities for ${this.name} are drawn from client model`);
+                            meta.capabilities = this.capabilities;
+                        }
+                        else {
+                            meta.capabilities = {};
+                        }
+                    }
+                    meta.capabilities = new Capabilities(meta.capabilities);
+
+                    // give each object a reference to meta
+                    items.forEach((a) => a[metaKey] = meta);
+
+                    response.data.data = items;
+
+                    return response;
+                }
+
+                static makeFromApiWithType(Type) {
+                    return function (resource) {
+                        return ApiBase.makeFromApi.call(Type, resource);
+                    };
+                }
+
+
             }
-        ])
+
+            return ApiBase;
+        }
+    ])
     .factory(
         "baw.models.ModelUnavailable",
         [
@@ -364,41 +363,34 @@ angular
                 function autoDownload(targetType, arity = arityOne, limit = []) {
                     throw new Error("Not Implemented");
                     /*
-                    // get associated models
-                    let targetAssociation = associations.get(targetType),
-                        models = [];
-                    if (targetAssociation.parents) {
-                        models = models.concat(targetAssociation.parents);
-                    }
-                    if (targetAssociation.children) {
-                        models = models.concat(targetAssociation.children);
-                    }
-
-                    models = models
-                        .filter(p => arity === arityMany || getArity(p) === arityOne)
-                        .filter(p => limit.length === 0 || limit.indexOf(getName(p)) >= 0);
-
-                    // get linkers
-                    let linkers = models.map( model => generateLinker(targetType, model) );
-
-                    // TODO: this won't work until services are expressed as Service functions
-                    let getService = () => { };
-
-                    for (let i = 0; i < models.length; i++) {
-
-
-                        let model = models[i],
-                            arity = getArity(model),
-                            service = getService(getName(model));
-
-                        if (arity === arityMany) {
-                            service.filter()
-                        }
-                        else {
-                            service.get()
-                        }
-                    }
-                    */
+                     // get associated models
+                     let targetAssociation = associations.get(targetType),
+                     models = [];
+                     if (targetAssociation.parents) {
+                     models = models.concat(targetAssociation.parents);
+                     }
+                     if (targetAssociation.children) {
+                     models = models.concat(targetAssociation.children);
+                     }
+                     models = models
+                     .filter(p => arity === arityMany || getArity(p) === arityOne)
+                     .filter(p => limit.length === 0 || limit.indexOf(getName(p)) >= 0);
+                     // get linkers
+                     let linkers = models.map( model => generateLinker(targetType, model) );
+                     // TODO: this won't work until services are expressed as Service functions
+                     let getService = () => { };
+                     for (let i = 0; i < models.length; i++) {
+                     let model = models[i],
+                     arity = getArity(model),
+                     service = getService(getName(model));
+                     if (arity === arityMany) {
+                     service.filter()
+                     }
+                     else {
+                     service.get()
+                     }
+                     }
+                     */
                 }
 
 
@@ -424,7 +416,7 @@ angular
 
                     let cacheKey = child + "---->" + parent;
                     if (linkerCache.has(cacheKey)) {
-                       return linkerCache.get(cacheKey);
+                        return linkerCache.get(cacheKey);
                     }
 
                     if (!associations.has(child)) {
@@ -442,11 +434,11 @@ angular
                     // select the shortest path
                     var {index} = chains.reduce(({length, index}, current, currentIndex) => {
                         if (current && current.length < length) {
-                            length = current.length;
-                            index = currentIndex;
-                        }
-                        return {length, index};
-                    }, {length: +Infinity, index: -1});
+                        length = current.length;
+                        index = currentIndex;
+                    }
+                    return {length, index};
+                }, {length: +Infinity, index: -1});
 
                     var chain = chains[index],
                         correctCase = chain.map(key => key[0].toLowerCase() + key.slice(1));
@@ -458,7 +450,7 @@ angular
                         var currentTargets = [target];
                         for (let c = 1; c < chain.length; c++) {
                             let association = chain[c],
-                            // check if the current target could have many parents or children
+                                // check if the current target could have many parents or children
                                 manyTargets = isManyAssociation(associations.get(chain[c - 1]), association),
                                 targetIdName = correctCase[c] + (manyTargets ? parentManyRelationSuffix : parentRelationSuffix),
                                 targetName = correctCase[c] + (manyTargets ? pluralitySuffix : "");
@@ -593,7 +585,7 @@ angular
                 function arrayToMap(items) {
                     return new Map(
                         items.map(item => [item[id], item])
-                    );
+                );
                 }
 
             }
