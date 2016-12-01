QutEcoacoustics/baw-client

commit fcb182fef592a1b690012f24080ef44dcb432ede

chore(code): Fixed bad multifile variable in directives files

src/components/directives/directives.js
@@ -1,352 +1,15 @@
-var bawds = bawds || angular.module("bawApp.directives", [
+angular.module("bawApp.directives",
+    [
         "bawApp.configuration",
         "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"
+        "bawApp.directives.formChildrenHack",
+        "bawApp.directives.notInListValidator",
+
+        "bawApp.directives.others",
+        "bawApp.directives.ngEval",
+        "bawApp.directives.ngAudio",
+        "bawApp.directives.inputRange",
+        "bawApp.directives.toggleSwitch",
+        "bawApp.directives.ngForm",
     ]);
-
-
-bawds.directive("bawRecordInformation", function () {
-
-    return {
-        restrict: "AE",
-        scope: false,
-        /* priority: ???  */
-//            controller: 'RecordInformationCtrl',
-        /* require: ??? */
-        /*template: "<div></div>",*/
-        templateUrl: "/assets/record_information.html",
-        replace: false,
-        /*compile: function(tElement, tAttrs, transclude) {
-
-         },*/
-        link: function (scope, iElement, iAttrs, controller) {
-            scope.name = scope[iAttrs.ngModel];
-
-
-        }
-
-    };
-});
-
-bawds.directive("bawJsonBinding", function () {
-    return {
-        restrict: "A",
-        require: "ngModel",
-        link: function (scope, element, attr, ngModel) {
-
-            function catchParseErrors(viewValue) {
-                var result;
-                try {
-                    result = angular.fromJson(viewValue);
-                } catch (e) {
-                    ngModel.$setValidity("bawJsonBinding", false);
-                    return "";
-                }
-                ngModel.$setValidity("bawJsonBinding", true);
-                return result;
-            }
-
-            ngModel.$parsers.push(catchParseErrors);
-            ngModel.$formatters.push(angular.toJson);
-        }
-    };
-});
-
-// ensures formatters are run on input blur
-bawds.directive("renderOnBlur", function () {
-    return {
-        require: "ngModel",
-        restrict: "A",
-        link: function (scope, elm, attrs, ctrl) {
-            elm.bind("blur", function () {
-                var viewValue = ctrl.$modelValue;
-                for (var i in ctrl.$formatters) { // jshint ignore:line
-                    viewValue = ctrl.$formatters[i](viewValue);
-                }
-                ctrl.$viewValue = viewValue;
-                ctrl.$render();
-            });
-        }
-    };
-});
-
-
-bawds.directive("isGuid", function () {
-    return {
-
-        require: "ngModel",
-        link: function (scope, elm, attrs, ctrl) {
-            var isList = typeof attrs.ngList !== "undefined";
-
-            // push rather than unshift... we want to test last
-            ctrl.$parsers.push(function (viewValue) {
-                var valid = true;
-                if (isList) {
-                    for (var i = 0; i < viewValue.length && valid; i++) {
-                        valid = baw.GUID_REGEXP.test(viewValue[i]);
-                    }
-                }
-                else {
-                    valid = baw.GUID_REGEXP.test(viewValue);
-                }
-
-                if (valid) {
-                    // it is valid
-                    ctrl.$setValidity("isGuid", true);
-                    return viewValue;
-                } else {
-                    // it is invalid, return undefined (no model update)
-                    ctrl.$setValidity("isGuid", false);
-                    return undefined;
-                }
-            });
-        }
-    };
-});
-
-// implements infinite scrolling
-// http://jsfiddle.net/vojtajina/U7Bz9/
-bawds.directive("whenScrolled", function () {
-    return function (scope, elm, attr) {
-        var raw = elm[0];
-
-        elm.bind("scroll", function () {
-            console.log("scrolled");
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-    };
-});
-
-
-/**
- * A cross record element checker
- */
-bawds.directive("bawChecked", ["$parse", function ($parse) {
-
-    // a cache of elements for each radio group
-    var library = {};
-
-
-    return {
-        restrict: "A",
-        link: function radioInputType(scope, element, attr) {
-            // make the name unique, if not defined
-            if (baw.angularCopies.isUndefined(attr.name)) {
-                element.attr("name", Number.Unique());
-            }
-
-            var getter = $parse(attr.bawChecked);
-            var assigner = getter.assign;
-
-            // store elements from same group to enable mass updates
-            library[attr.name] = library[attr.name] || [];
-            library[attr.name].push({e: element, s: scope, g: getter, a: assigner});
-
-
-            // forward binding from model
-            // aggressively updates all elements
-            scope.$watch(getter, function (newValue, oldValue) {
-                if (newValue) {
-                    // if a true value is set, aggressively set others to false
-                    angular.forEach(library[attr.name], function (libraryItem) {
-                        if (libraryItem.e === element) {
-                            libraryItem.e[0].checked = true;
-                        } else {
-                            libraryItem.e[0].checked = false;
-                            libraryItem.a(libraryItem.s, false);
-                        }
-                    });
-                }
-                else {
-                    // if it's false, just make sure it is checked right (don't perpetuate the loop)
-                    element[0].checked = false;
-                }
-            });
-
-            // reverse bindings, elements to model
-            function updateModel(event) {
-                var isChecked = event.target.checked;
-
-                var newest = library[attr.name][library[attr.name].length - 1];
-                // STUPID ARSE HACK... for some reason the latest item although selected does not properly update
-                // the 'last' value on the watch. so when it is first set to false nothing happens.
-                // therefore *force* a change here by doing a separate apply with a bullshit value
-                scope.$apply(function () {
-                    newest.a(newest.s, null);
-                });
-
-                scope.$apply(function () {
-                    assigner(scope, isChecked);
-
-                    if (newest.e[0] !== event.target) {
-                        newest.a(newest.s, !isChecked);
-                    }
-                });
-
-            }
-
-            element.bind("click", updateModel);
-
-        }
-    };
-}]);
-
-
-/**
- * A directive for binding the position of an element to the model.
- * The binding uses translate transforms.
- *
- * ONLY one way binding supported at the moment
- */
-bawds.directive("bawTranslateX", function () {
-
-    function getSupportedTransform() {
-        var prefixes = "transform WebkitTransform MozTransform OTransform msTransform".split(" ");
-        var div = document.createElement("div");
-        for(var i = 0; i < prefixes.length; i++) {
-            if(div && div.style[prefixes[i]] !== undefined) {
-                return prefixes[i];
-            }
-        }
-        return false;
-    }
-
-    var transformProperty = getSupportedTransform();
-    if (!transformProperty) {
-        transformProperty = "left";
-    }
-
-    return {
-        restrict: "A",
-        link: function (scope, elements, attributes, controller) {
-            var element = elements[0];
-
-            function leftWatcher(newValue, oldValue) {
-                element.style[transformProperty] = "" + newValue.toFixed(3) + "px";
-            }
-
-            function transformWatcher(newValue, oldValue) {
-                element.style[transformProperty] = "translate3d(" + newValue.toFixed(3) + "px, 0, 0)";
-            }
-
-            var watcher = getSupportedTransform() ? transformWatcher : leftWatcher;
-
-            scope.$watch(attributes.bawTranslateX, watcher);
-        }
-    };
-});
-
-bawds.directive("bawImageLoaded", ["$timeout", "$parse", function ($timeout, $parse) {
-    return {
-        restrict: "A",
-        link: function (scope, elements, attr) {
-            var element = elements[0];
-            if (element.nodeName !== "IMG") {
-                throw "Cannot put bawImageLoaded element on an element that is not a <image />";
-            }
-
-            var getter = $parse(attr.bawImageLoaded);
-            var assigner = getter.assign;
-
-            assigner(scope, element.complete);
-
-            element.onload = function () {
-                assigner(scope, element.complete);
-            };
-/*
-            function checkLater() {
-                $timeout(function () {
-                    assigner(scope, element.complete);
-
-                    //if (element.complete) {
-                    checkLater();
-                    //}
-                }, 1000);
-            }*/
-
-        }
-    };
-}]);
-
-bawds.directive("bawInjectTransformers", function () {
-    return {
-        restrict: "A",
-        require: "ngModel",
-        priority: -1,
-        link: function (scope, element, attr, ngModel) {
-            var local = scope.$eval(attr.bawInjectTransformers);
-
-            if (!angular.isObject(local) || !angular.isFunction(local.fromModel) || !angular.isFunction(local.fromElement)) {
-                throw "The bawInjectTransformers must be bound to an object with two functions (`fromModel` and `fromElement`)";
-            }
-
-            ngModel.$parsers.push(local.fromElement);
-            ngModel.$formatters.push(local.fromModel);
-        }
-    };
-});
-
-/*bawds.directive('bawMediaPlayer', function () {
- return {
- restrict: 'EA',
- template: '<div></div>',
- link: function (scope, element, attrs) {
- var $control = element,
- $player = $control.children('div'),
- cls = 'pause';
-
- var updatePlayer = function () {
- $player.jPlayer({
- // Flash fallback for outdated browser not supporting HTML5 audio/video tags
- // http://jplayer.org/download/
- swfPath: 'assets/',
- supplied: 'mp3,oga,webma',
- nativeSupport: true,
- oggSupport: true,
- solution: 'html, flash',
- preload: 'auto',
- wmode: 'window',
- ready: function () {
- $player
- .jPlayer("setMedia", {
- mp3: attrs.audiomp3,
- oga: attrs.audiooga,
- webma: attrs.audiowebma
- })
- .jPlayer(attrs.autoplay === 'true' ? 'play' : 'stop');
- },
- play: function () {
- $control.addClass(cls);
-
- if (attrs.pauseothers === 'true') {
- $player.jPlayer('pauseOthers');
- }
- },
- pause: function () {
- $control.removeClass(cls);
- },
- stop: function () {
- $control.removeClass(cls);
- },
- ended: function () {
- $control.removeClass(cls);
- }
- })
- .end()
- .unbind('click').click(function (e) {
- $player.jPlayer($control.hasClass(cls) ? 'stop' : 'play');
- });
- };
-
- scope.$watch(attrs.audio, updatePlayer);
- updatePlayer();
- }
- };
- });*/
-
-
-
-
 

src/components/directives/bawAnnotationViewer.js
@@ -1,9 +1,7 @@
-var bawds = bawds || angular.module("bawApp.directives", [
-        "bawApp.configuration", 
-        "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"]);
+angular
+    .module("bawApp.directives.annotationViewer", [])
 
-bawds.directive("bawAnnotationViewer",
+    .directive("bawAnnotationViewer",
     [   "conf.paths",
         "bawApp.unitConverter",
         "AudioEvent",

src/components/directives/input[type=range].js
@@ -1,39 +1,36 @@
-var bawds = bawds || angular.module("bawApp.directives", [
-        "bawApp.configuration", 
-        "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"]);
+angular
+    .module("bawApp.directives.inputRange", [])
+    .directive("ngSlider", function () {
+        return {
+            restrict: "A",
+            link: function (scope, $element, attrs) {
+                var element = $element[0];
 
-bawds.directive("ngSlider", function () {
-    return {
-        restrict: "A",
-        link: function (scope, $element, attrs) {
-            var element = $element[0];
+                var last = null;
+                var updateScope = function () {
+                    if (element.value === last) {
+                        return;
+                    }
 
-            var last = null;
-            var updateScope = function () {
-                if (element.value === last) {
-                    return;
-                }
+                    //console.debug("ngSlider:updateScope: Calling apply!", element.value);
 
-                //console.debug("ngSlider:updateScope: Calling apply!", element.value);
+                    scope.$apply(function () {
+                        scope[attrs.ngSlider] = element.value;
+                    });
+                };
 
-                scope.$apply(function () {
-                    scope[attrs.ngSlider] = element.value;
+                // IE does not use the input event like other browsers
+                // so bind to both and try and drop events by checking for unnecessary change events
+                element.addEventListener("change", updateScope);
+                element.addEventListener("input", updateScope);
+
+                scope.$watch(function () {
+                    return scope[attrs.ngSlider];
+                }, function (newValue) {
+                    element.value = last = newValue;
                 });
-            };
-
-            // IE does not use the input event like other browsers
-            // so bind to both and try and drop events by checking for unnecessary change events
-            element.addEventListener("change", updateScope);
-            element.addEventListener("input", updateScope);
-
-            scope.$watch(function () {
-                return scope[attrs.ngSlider];
-            }, function (newValue) {
-                element.value = last = newValue;
-            });
 
 
-        }
-    };
-});
+            }
+        };
+    });

src/components/directives/ngAudio.js
@@ -1,14 +1,13 @@
-var ngAudio = ngAudio || angular.module("bawApp.directives.ngAudio", [
-        "bawApp.configuration", 
-        "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"]);
+angular.module("bawApp.directives.ngAudio", [
+        "bawApp.configuration"
+])
 
 
-ngAudio.constant("ngAudioEvents", {
+.constant("ngAudioEvents", {
     volumeChanged: "ngAudio:volumeChanged",
     muteChanged: "ngAudio:muted",
     ended: "ended"
-});
+})
 
 /**
  * A directive for binding the model to data off an audio element.
@@ -18,7 +17,7 @@ ngAudio.constant("ngAudioEvents", {
  * like a "isBuffering" binding.
  */
 
-ngAudio.directive("ngAudio", ["ngAudioEvents", "$parse", function (ngAudioEvents, $parse) {
+.directive("ngAudio", ["ngAudioEvents", "$parse", function (ngAudioEvents, $parse) {
 
     /* const */ var readyStates = {
         "haveNothing": 0,

src/components/directives/ngEval.js
@@ -1,17 +1,14 @@
-var bawds = bawds || angular.module("bawApp.directives", [
-        "bawApp.configuration", 
-        "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"]);
+angular
+    .module("bawApp.directives.ngEval", [])
+    .directive("ngEval", function () {
+        return {
+            restrict: "A",
+            link: function (scope, element, attrs) {
 
-bawds.directive("ngEval", function () {
-    return {
-        restrict: "A",
-        link: function (scope, element, attrs) {
-
-            // HACK: this is a horrible, horrible way to do this!
-            // Watcher methods should be idempotent - eval is meant to cause side effects
-            scope.$watch(attrs.ngEval, function() {
-            });
-        }
-    };
-});
+                // HACK: this is a horrible, horrible way to do this!
+                // Watcher methods should be idempotent - eval is meant to cause side effects
+                scope.$watch(attrs.ngEval, function () {
+                });
+            }
+        };
+    });

src/components/directives/ngForm.js
@@ -1,0 +1,55 @@
+angular
+    .module("bawApp.directives.ngForm", [])
+    .directive("form", function() {
+        // dirty hack until https://github.com/angular/angular.js/issues/14749 is a thing
+        return {
+            require: "form",
+            link:  {
+                // use pre-linking function so we can wrap methods before any controls are added
+                pre: function(scope, elm, attrs, ctrl) {
+                    if (ctrl.$getControls) {
+                        throw new Error("Angular finally allowed $getControls on FormController");
+                    }
+
+                    var controls = [],
+                        originalAdd = ctrl.$addControl,
+                        originalRemove = ctrl.$removeControl;
+
+                    ctrl.$addControl = function(control) {
+                        //console.debug("Test controller extension", control);
+
+                        controls.push(control);
+
+                        originalAdd.call(ctrl, control);
+                    };
+
+                    ctrl.$removeControl = function(control) {
+                        arrayRemove(controls, control);
+
+                        originalRemove.call(ctrl, control);
+                    };
+
+                    ctrl.$getControls = function() {
+                        return controls;
+                    };
+
+                    ctrl.$validateChildren = function () {
+                        for (var control of controls) {
+                            if (control) {
+                                //console.debug("Validating: " + control.$name);
+                                control.$validate();
+                            }
+                        }
+                    };
+
+                    function arrayRemove(array, value) {
+                        var index = array.indexOf(value);
+                        if (index >= 0) {
+                            array.splice(index, 1);
+                        }
+                        return index;
+                    }
+                }
+            }
+        };
+    });

src/components/directives/ngGoogleMaps.js
@@ -1,145 +1,0 @@
-/*
- * NOTE: this was copied from angular-ui. At some point this file should be deleted.
- */
-
-/* globals google*/
-
-var bawds = bawds || angular.module("bawApp.directives", [
-        "bawApp.configuration",
-        "bawApp.directives.ui.bootstrap",
-        "bawApp.directives.formChildrenHack"
-    ]);
-
-/* Start map directives */
-/** stolen from angular ui
- * https://github.com/angular-ui/angular-ui/commit/d77bfe74e4ca2c463f76bac4f8b2e1e7464f7773#modules/directives/map/map.js
- */
-    //Setup map events from a google map object to trigger on a given element too,
-    //then we just use ui-event to catch events from an element
-function bindMapEvents(scope, eventsStr, googleObject, element) {
-    angular.forEach(eventsStr.split(" "), function (eventName) {
-        //Prefix all googlemap events with 'map-', so eg 'click'
-        //for the googlemap doesn't interfere with a normal 'click' event
-
-        var $event = {
-            type: "map-" + eventName
-        };
-        google.maps.event.addListener(googleObject, eventName, function (evt) {
-            element.triggerHandler(angular.extend({}, $event, evt));
-            //We create an $apply if it isn't happening. we need better support for this
-            //We don't want to use timeout because tons of these events fire at once,
-            //and we only need one $apply
-            if (!scope.$$phase) {
-                scope.$apply();
-            }
-        });
-
-        var $eventOnce = {
-            type: "map-once-" + eventName
-        };
-        google.maps.event.addListenerOnce(googleObject, eventName, function (evt) {
-            element.triggerHandler(angular.extend({}, $eventOnce, evt));
-            console.log("addListenerOnce", $eventOnce);
-            //We create an $apply if it isn't happening. we need better support for this
-            //We don't want to use timeout because tons of these events fire at once,
-            //and we only need one $apply
-            if (!scope.$$phase) {
-                scope.$apply();
-            }
-        });
-
-    });
-}
-
-bawds.directive("bawMap", ["ui.config", "$parse", function (uiConfig, $parse) {
-
-    var mapEvents = "bounds_changed center_changed click dblclick drag dragend " +
-        "dragstart heading_changed idle maptypeid_changed mousemove mouseout " +
-        "mouseover projection_changed resize rightclick tilesloaded tilt_changed " +
-        "zoom_changed";
-    var options = uiConfig.map || {};
-
-    return {
-        restrict: "A",
-        //doesn't work as E for unknown reason
-        link: function (scope, elm, attrs) {
-            var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
-            var map = new google.maps.Map(elm[0], opts);
-            var model = $parse(attrs.bawMap);
-
-            //Set scope variable for the map
-            model.assign(scope, map);
-
-            bindMapEvents(scope, mapEvents, map, elm);
-        }
-    };
-}]);
-
-bawds.directive("bawMapInfoWindow", ["ui.config", "$parse", "$compile", function (uiConfig, $parse, $compile) {
-
-    var infoWindowEvents = "closeclick content_change domready " +
-        "position_changed zindex_changed";
-    var options = uiConfig.mapInfoWindow || {};
-
-    return {
-        link: function (scope, elm, attrs) {
-            var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
-            opts.content = elm[0];
-            var model = $parse(attrs.bawMapInfoWindow);
-            var infoWindow = model(scope);
-
-            if (!infoWindow) {
-                infoWindow = new google.maps.InfoWindow(opts);
-                model.assign(scope, infoWindow);
-            }
-
-            bindMapEvents(scope, infoWindowEvents, infoWindow, elm);
-
-            /* The info window's contents dont' need to be on the dom anymore,
-             google maps has them stored. So we just replace the infowindow element
-             with an empty div. (we don't just straight remove it from the dom because
-             straight removing things from the dom can mess up angular) */
-            elm.replaceWith("<div></div>");
-
-            //Decorate infoWindow.open to $compile contents before opening
-            var _open = infoWindow.open;
-            infoWindow.open = function open(a1, a2, a3, a4, a5, a6) {
-                $compile(elm.contents())(scope);
-                _open.call(infoWindow, a1, a2, a3, a4, a5, a6);
-            };
-        }
-    };
-}]);
-
-/*
- * Map overlay directives all work the same. Take map marker for example
- * <ui-map-marker="myMarker"> will $watch 'myMarker' and each time it changes,
- * it will hook up myMarker's events to the directive dom element. Then
- * ui-event will be able to catch all of myMarker's events. Super simple.
- */
-function mapOverlayDirective(directiveName, events) {
-    bawds.directive(directiveName, [function () {
-        return {
-            restrict: "A",
-            link: function (scope, elm, attrs) {
-                scope.$watch(attrs[directiveName], function (newObject) {
-                    bindMapEvents(scope, events, newObject, elm);
-                });
-            }
-        };
-    }]);
-}
-
-mapOverlayDirective("bawMapMarker", "animation_changed click clickable_changed cursor_changed " + "dblclick drag dragend draggable_changed dragstart flat_changed icon_changed " + "mousedown mouseout mouseover mouseup position_changed rightclick " + "shadow_changed shape_changed title_changed visible_changed zindex_changed");
-
-mapOverlayDirective("bawMapPolyline", "click dblclick mousedown mousemove mouseout mouseover mouseup rightclick");
-
-mapOverlayDirective("bawMapPolygon", "click dblclick mousedown mousemove mouseout mouseover mouseup rightclick");
-
-mapOverlayDirective("bawMapRectangle", "bounds_changed click dblclick mousedown mousemove mouseout mouseover " + "mouseup rightclick");
-
-mapOverlayDirective("bawMapCircle", "center_changed click dblclick mousedown mousemove " + "mouseout mouseover mouseup radius_changed rightclick");
-
-mapOverlayDirective("bawMapGroundOverlay", "click dblclick");
-
-/* End map directives */

commit a02305ecc6034579eeddb656b02005514ff41663

fix(annotationViewer): Readded the module back in!
Broken in fcb182fef592a1b690012f24080ef44dcb432ede

src/components/directives/directives.js
@@ -4,7 +4,7 @@ angular.module("bawApp.directives",
         "bawApp.directives.ui.bootstrap",
         "bawApp.directives.formChildrenHack",
         "bawApp.directives.notInListValidator",
-
+        "bawApp.directives.annotationViewer",
         "bawApp.directives.others",
         "bawApp.directives.ngEval",
         "bawApp.directives.ngAudio",