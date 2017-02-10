QutEcoacoustics/baw-client

commit cd00962515ba14ca3cd4b47a1696de15186f858f

feat(queryBuilder): toJSON standardized
The QueryBuilder's toJSON method returned a JSON encoded string - rather
than the filtered object it should return. By changing it, the browser's
built JSON.stringfy method does the hard work when it serializes the
object (because it calls toJSON).

src/components/services/audioEvent.js
@@ -77,7 +77,7 @@ angular
                         return q;
                     });
 
-                    return $http.post(url, qb.toJSON());
+                    return $http.post(url, qb.toJSONString());
                 };
 
                 const filterUrl = paths.api.routes.audioEvent.filterAbsolute;
@@ -87,7 +87,7 @@ angular
                     });
 
                     return $http
-                        .post(filterUrl, query.toJSON())
+                        .post(filterUrl, query.toJSONString())
                         .then(x => AudioEventModel.makeFromApi(x));
                 };
 
@@ -100,7 +100,7 @@ angular
                         );
                     });
 
-                    return $http.post(filterUrl, query.toJSON())
+                    return $http.post(filterUrl, query.toJSONString())
                         .then(resultPager.loadAll)
                         .then(x => AudioEventModel.makeFromApi(x));
                 };

src/components/services/queryBuilder.js
@@ -417,7 +417,7 @@ angular
                         return this.end.call(query);
                     };
 
-                    this.toJSON = function toJSON(spaces) {
+                this.toJSON = function() {
                         var compiledQuery = {},
                             that = this;
 
@@ -427,7 +427,11 @@ angular
                             }
                         });
 
-                        return JSON.stringify(compiledQuery, null, spaces);
+                    return compiledQuery;
+                };
+
+                this.toJSONString = function toJSON(spaces) {
+                    return JSON.stringify(this.toJSON(), null, spaces);
                     };
 
                     this.toQueryString = function toQueryString() {

src/components/services/tag.js
@@ -130,7 +130,7 @@ angular
                         return q.in("audioEvents.id", audioEventIds);
                     });
 
-                    return $http.post(url, query.toJSON()).then(x => TagModel.makeFromApi(x));
+                    return $http.post(url, query.toJSONString()).then(x => TagModel.makeFromApi(x));
                 };
 
                 return resource;

src/components/services/audioRecording.js
@@ -30,7 +30,7 @@ angular
                 resource.getRecentRecordings = function () {
 
 
-                    return $http.post(filterUrl, query.toJSON());
+                    return $http.post(filterUrl, query.toJSONString());
                 };
 
                 resource.getRecordingsForVisualization = function (siteIds) {
@@ -45,7 +45,7 @@ angular
                     });
 
                     return $http
-                        .post(filterUrl, query.toJSON())
+                        .post(filterUrl, query.toJSONString())
                         .then(x => AudioRecordingCore.makeFromApi(x));
                 };
 
@@ -55,7 +55,7 @@ angular
                         .project({include: ["id", "siteId", "durationSeconds", "recordedDate"]}));
 
                     return $http
-                        .post(filterUrl, query.toJSON())
+                        .post(filterUrl, query.toJSONString())
                         .then(x => AudioRecordingModel.makeFromApi(x));
                 };
                 return resource;

src/components/services/project.js
@@ -32,7 +32,7 @@ angular
                 });
                 resource.getAllProjects = function () {
                     return $http
-                        .post(gapUrl, gapQuery.toJSON())
+                        .post(gapUrl, gapQuery.toJSONString())
                         .then(x => ProjectModel.makeFromApi(x));
                 };
 
@@ -47,7 +47,7 @@ angular
                     });
 
                     return $http
-                        .post(gpbiUrl, query.toJSON())
+                        .post(gpbiUrl, query.toJSONString())
                         .then(x => ProjectModel.makeFromApi(x));
 
                 };
@@ -61,7 +61,7 @@ angular
                     });
 
                     return $http
-                        .post(gpbsiUrl, query.toJSON())
+                        .post(gpbsiUrl, query.toJSONString())
                         .then(x => ProjectModel.makeFromApi(x));
                 };
 

src/components/services/queryBuilder.spec.js
@@ -106,7 +106,7 @@ describe("The QueryBuilder", function () {
 
         var actual = q.compose(q.or(q.eq("field", 3.0), q.lt("field", 6.0)));
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("will gracefully resolve key conflicts for combinators in the deep merge - test 2", function() {
@@ -133,7 +133,7 @@ describe("The QueryBuilder", function () {
             return q;
         });
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("a query operator should return a new instance of a Query", function () {
@@ -170,7 +170,7 @@ describe("The QueryBuilder", function () {
     it("should be able to produce a bare query", function () {
         var expected = {};
 
-        expect(q.toJSON(spaces)).toBe(j(expected));
+        expect(q.toJSONString(spaces)).toBe(j(expected));
     });
 
 
@@ -187,7 +187,7 @@ describe("The QueryBuilder", function () {
 
         var actual = q.compose(q.and(q.eq("field", 3.0)));
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should ensure .end and .compose and .create are the same", function () {
@@ -206,16 +206,16 @@ describe("The QueryBuilder", function () {
         };
 
         var actualCompose = q.compose(q.eq("fieldA", 3.0).or(q.field("fieldB").lt(6.0).gt(3.0)));
-        expect(actualCompose.toJSON(spaces)).toBe(j(expected));
+        expect(actualCompose.toJSONString(spaces)).toBe(j(expected));
 
         q = queryBuilder.create();
         var actualEnd = q.eq("fieldA", 3.0).or(q.field("fieldB").lt(6.0).gt(3.0)).end();
-        expect(actualEnd.toJSON(spaces)).toBe(j(expected));
+        expect(actualEnd.toJSONString(spaces)).toBe(j(expected));
 
         var actualCreate = queryBuilder.create(function (q) {
             return q.eq("fieldA", 3.0).or(q.field("fieldB").lt(6.0).gt(3.0));
         });
-        expect(actualCreate.toJSON(spaces)).toBe(j(expected));
+        expect(actualCreate.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should ensure not is arity:1 only", function () {
@@ -235,7 +235,7 @@ describe("The QueryBuilder", function () {
 
         var actual = q.compose(q.eq("field", 3.0));
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should allow more than one operation at root level", function () {
@@ -252,7 +252,7 @@ describe("The QueryBuilder", function () {
 
         var actual = q.compose(q.eq("fieldA", 3.0).eq("fieldB", 6.0));
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("ensures that the in function only takes an array", function () {
@@ -276,7 +276,7 @@ describe("The QueryBuilder", function () {
 
         var actual = q.compose(q.in("fieldA", new Set([1, 2, 3, 3])));
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("ensures that the in function automatically does a uniqueness check", function () {
@@ -290,7 +290,7 @@ describe("The QueryBuilder", function () {
 
         var actual = q.compose(q.in("fieldA", [1, 2, 3, 3]));
 
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("ensures the regex function is not supported", function () {
@@ -316,7 +316,7 @@ describe("The QueryBuilder", function () {
 
                 var actual = q.compose(q[rangeFunction]("fieldA", "(3,20.0)"));
 
-                expect(actual.toJSON(spaces)).toBe(j(expected));
+                expect(actual.toJSONString(spaces)).toBe(j(expected));
 
                 expected.filter.fieldA[rangeFunction] = {
                     from: 3,
@@ -326,7 +326,7 @@ describe("The QueryBuilder", function () {
                 q = queryBuilder.create();
                 actual = q.compose(q[rangeFunction]("fieldA", {from: 3, to: 20.0}));
 
-                expect(actual.toJSON(spaces)).toBe(j(expected));
+                expect(actual.toJSONString(spaces)).toBe(j(expected));
             });
 
             it(rangeFunction + " it validates a string range", function () {
@@ -400,7 +400,7 @@ describe("The QueryBuilder", function () {
 
                 var actual = q.compose(q[rangeFunction]("fieldA", {from: 20, to: null}));
 
-                expect(actual.toJSON(spaces)).toBe(j(expected));
+                expect(actual.toJSONString(spaces)).toBe(j(expected));
 
                 expected = {
                     filter: {
@@ -415,7 +415,7 @@ describe("The QueryBuilder", function () {
                 q = queryBuilder.create();
                 actual = q.compose(q[rangeFunction]("fieldA", {from: null, to: 20}));
 
-                expect(actual.toJSON(spaces)).toBe(j(expected));
+                expect(actual.toJSONString(spaces)).toBe(j(expected));
 
                 expected = {
                 };
@@ -423,7 +423,7 @@ describe("The QueryBuilder", function () {
                 q = queryBuilder.create();
                 actual = q.compose(q[rangeFunction]("fieldA", {from: null, to: null}));
 
-                expect(actual.toJSON(spaces)).toBe(j(expected));
+                expect(actual.toJSONString(spaces)).toBe(j(expected));
             });
         });
 
@@ -437,11 +437,11 @@ describe("The QueryBuilder", function () {
             };
 
             var actual = q.compose(q.range("fieldA", "(,20.0)"));
-            expect(actual.toJSON(spaces)).toBe(j(expected));
+            expect(actual.toJSONString(spaces)).toBe(j(expected));
 
             q = queryBuilder.create();
             actual = q.compose(q.range("fieldA", {to: 20}));
-            expect(actual.toJSON(spaces)).toBe(j(expected));
+            expect(actual.toJSONString(spaces)).toBe(j(expected));
 
             expected = {
                 filter: {
@@ -453,7 +453,7 @@ describe("The QueryBuilder", function () {
 
             q = queryBuilder.create();
             actual = q.compose(q.range("fieldA", "(,20.0]"));
-            expect(actual.toJSON(spaces)).toBe(j(expected));
+            expect(actual.toJSONString(spaces)).toBe(j(expected));
         });
 
         it("the smart range function simplifies a missing upper bound", function () {
@@ -466,7 +466,7 @@ describe("The QueryBuilder", function () {
             };
             
             var actual = q.compose(q.range("fieldA", "(3,)"));
-            expect(actual.toJSON(spaces)).toBe(j(expected));
+            expect(actual.toJSONString(spaces)).toBe(j(expected));
 
 
             expected = {
@@ -479,11 +479,11 @@ describe("The QueryBuilder", function () {
 
             q = queryBuilder.create();
             actual = q.compose(q.range("fieldA", "[3,)"));
-            expect(actual.toJSON(spaces)).toBe(j(expected));
+            expect(actual.toJSONString(spaces)).toBe(j(expected));
 
             q = queryBuilder.create();
             actual = q.compose(q.range("fieldA", {from: 3}));
-            expect(actual.toJSON(spaces)).toBe(j(expected));
+            expect(actual.toJSONString(spaces)).toBe(j(expected));
         });
 
     });
@@ -509,7 +509,7 @@ describe("The QueryBuilder", function () {
         var actual = q.compose(q.and(
             q.lt("fieldA", 3.0).contains("fieldB", "hello").gt("fieldA", 0.0)
         ).lt("fieldC", 17));
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should handle a more complex query - with separate queries for the same field", function () {
@@ -531,7 +531,7 @@ describe("The QueryBuilder", function () {
             q.lt("fieldA", 3.0).contains("fieldB", "hello"),
             q.gt("fieldA", 0.0)
         ));
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should handle a more complex query - with lots of nesting", function () {
@@ -579,7 +579,7 @@ describe("The QueryBuilder", function () {
                 )
             )
         ));
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should allow paging to be set", function () {
@@ -591,7 +591,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.page({items: 10, page: 30});
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should validate page arguments", function () {
@@ -620,7 +620,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.page.disable();
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
 
@@ -633,7 +633,7 @@ describe("The QueryBuilder", function () {
            };
 
            var actual = q.page({}).page.disable();
-           expect(actual.toJSON(spaces)).toBe(j(expected));
+           expect(actual.toJSONString(spaces)).toBe(j(expected));
        });
 
     it("should should allow re-enabling of paging", function () {
@@ -646,7 +646,7 @@ describe("The QueryBuilder", function () {
 
         // this essentially represents paging with the default options
         var actual = q.page.disable().page({});
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
 
@@ -664,7 +664,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.eq("fieldA", 30).page({items: 5, page: 2}).end();
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should always update root with paging...even if on a subquery (for disablePaging too)", function () {
@@ -680,7 +680,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.eq("fieldA", 30).page.disable().end();
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should allow sorting to be set", function () {
@@ -692,7 +692,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.sort({orderBy: "durationSeconds", direction: "desc"});
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should validate sorting arguments", function () {
@@ -731,7 +731,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.lt("fieldB", 6.0).sort({orderBy: "durationSeconds"}).end();
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should allow projection to be set (whitelist)", function () {
@@ -744,7 +744,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.project({include: ["durationSeconds", "id"]});
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
 
     });
 
@@ -757,7 +757,7 @@ describe("The QueryBuilder", function () {
             }
         };
         var actual = q.project({exclude: ["durationSeconds", "id"]});
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
     it("should validate projection arguments", function () {
@@ -796,7 +796,7 @@ describe("The QueryBuilder", function () {
         };
 
         var actual = q.notEq("fieldC", 7.5).project({include: ["durationSeconds", "fieldC"]}).end();
-        expect(actual.toJSON(spaces)).toBe(j(expected));
+        expect(actual.toJSONString(spaces)).toBe(j(expected));
     });
 
 
@@ -918,7 +918,7 @@ describe("The QueryBuilder", function () {
             {page: 1, items: 10}
         ).end();
 
-        expect(actual.toJSON(spaces)).toBe(j(complexExpected));
+        expect(actual.toJSONString(spaces)).toBe(j(complexExpected));
     });
 
     it("should be able to load a very complex query", function () {

src/components/services/site.js
@@ -15,7 +15,7 @@ angular
                         .sort({orderBy: "name"});
                 });
                 return $http
-                    .post(url, query.toJSON())
+                    .post(url, query.toJSONString())
                     .then( x => SiteModel.makeFromApi(x));
             };
 
@@ -27,7 +27,7 @@ angular
                         .sort({orderBy: "name"});
                 });
                 return $http
-                    .post(url, query.toJSON())
+                    .post(url, query.toJSONString())
                     .then( x => SiteModel.makeFromApi(x));
             };
 
@@ -38,7 +38,7 @@ angular
                         .sort({orderBy: "name"});
                 });
                 return $http
-                    .post(url, query.toJSON())
+                    .post(url, query.toJSONString())
                     .then( x => SiteModel.makeFromApi(x));
             };
 
@@ -47,7 +47,7 @@ angular
              var query = QueryBuilder.create(function (q) {
              return q.project({"include": ["id", "name"]});
              });
-             return $http.post(url, query.toJSON());
+             return $http.post(url, query.toJSONString());
              };*/
 
 

commit 7a678bf2b3802e73d4af52353b931c53bb5064c3

fix(API): Fix various premature string conversions
API data objects were being prematurely converted to a string and were
bypassing the casing renamer http transformer.

Fixes #280

Original bug introduced in
https://github.com/QutBioacoustics/baw-client/commit/cd00962515ba14ca3cd4b47a1696de15186f858f

src/app/listen/listen.js
@@ -614,7 +614,7 @@ angular
                             function success(value, headers) {
 
                                 // possible race condition: may no longer may be selected after async
-                                $scope.model.selectedAudioEvent.taggings[index] = new baw.Tagging(value);
+                                $scope.model.selectedAudioEvent.taggings[index] = new baw.Tagging(value.data);
                                 $scope.model.selectedAudioEvent.tags[index] = addedTag.tag;
 
                                 console.assert(

src/components/services/audioEvent.js
@@ -77,7 +77,7 @@ angular
                         return q;
                     });
 
-                    return $http.post(url, qb.toJSONString());
+                    return $http.post(url, qb.toJSON());
                 };
 
                 const filterUrl = paths.api.routes.audioEvent.filterAbsolute;
@@ -87,7 +87,7 @@ angular
                     });
 
                     return $http
-                        .post(filterUrl, query.toJSONString())
+                        .post(filterUrl, query.toJSON())
                         .then(x => AudioEventModel.makeFromApi(x));
                 };
 
@@ -100,7 +100,7 @@ angular
                         );
                     });
 
-                    return $http.post(filterUrl, query.toJSONString())
+                    return $http.post(filterUrl, query.toJSON())
                         .then(resultPager.loadAll)
                         .then(x => AudioEventModel.makeFromApi(x));
                 };

src/components/services/queryBuilder.js
@@ -381,7 +381,7 @@ angular
 
                 function filterToQueryString(filterObject) {
                     var results = [];
-                    for(let [key, value] of Object.entries(filterObject)) {
+                    for (let [key, value] of Object.entries(filterObject)) {
                         if (!validCombinators.hasOwnProperty(key)) {
                             if (value.hasOwnProperty("eq")) {
                                 results.push(["filter_" + key, value.eq]);
@@ -417,7 +417,7 @@ angular
                         return this.end.call(query);
                     };
 
-                this.toJSON = function() {
+                    this.toJSON = function () {
                         var compiledQuery = {},
                             that = this;
 
@@ -427,11 +427,11 @@ angular
                             }
                         });
 
-                    return compiledQuery;
-                };
+                        return compiledQuery;
+                    };
 
-                this.toJSONString = function toJSON(spaces) {
-                    return JSON.stringify(this.toJSON(), null, spaces);
+                    this.toJSONString = function toJSON(spaces) {
+                        return JSON.stringify(this.toJSON(), null, spaces);
                     };
 
                     this.toQueryString = function toQueryString() {

src/components/services/tag.js
@@ -130,7 +130,7 @@ angular
                         return q.in("audioEvents.id", audioEventIds);
                     });
 
-                    return $http.post(url, query.toJSONString()).then(x => TagModel.makeFromApi(x));
+                    return $http.post(url, query.toJSON()).then(x => TagModel.makeFromApi(x));
                 };
 
                 return resource;