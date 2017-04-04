f8b15601be58211777739d0342dfafa2db6bc2ec
Phil <phil.eichinski@gmail.com>
2017-04-04 17:17:48 +1000

    fix(background): addresses comments to pull request

--- src/components/services/background/background.js
+++ src/components/services/background/background.js
@@ -1,103 +1,105 @@
 angular.module("bawApp.components.background", [])
-    .service("bgImage", function() {
-        var curBg = "";
+    .service("backgroundImage", function () {
+        var currentBackground = "";
         return {
-            curBg: curBg
+            currentBackground: currentBackground
         };
     })
-    .component("background",{
-        template: "<div ng-repeat='bg in bgs' ng-if=\"bg.state !== 'finished'\" class='background {{bg.state}}' style='background-image:url({{bg.url}});'></div>",
-        controller: ["$scope", "bgImage", function ($scope, bgImage) {
+    .component("background", {
+        template: `<div ng-repeat='background in backgrounds' ng-if="background.state !== 'finished'" class='background {{background.state}}' style='background-image:url({{background.url}});'></div>`,
+        controller: ["$scope", "backgroundImage", "$timeout", function ($scope, backgroundImage, $timeout) {
 
             var self = this;
 
-            $scope.bgs = [];
+            $scope.backgrounds = [];
 
             /**
-             * Watches for changes to the bgImage service. When a change is detected it
-             * adds to the bgs array on scope. The array holds the url and the 'state'. State can be
+             * Watches for changes to the backgroundImage service. When a change is detected it
+             * adds to the backgrounds array on scope. The array holds the url and the 'state'. State can be
              * preloading, fadeIn, done and finished.
              * preloading: image is preloading. Set the opacity to 0
              * fadeIn: image has been preloaded, fade it in.
              * done: image is finished fading in. Anything behind it can be removed.
              * finished: is behind an image with state done. Remove it from dom.
-             * The bgs array is bound to scope in a repeat. New images can be added quicker than they can fade in without problem
+             * The backgrounds array is bound to scope in a repeat. New images can be added quicker than they can fade in without problem
              * because it just adds them over the top of the existing fading-in images.
              * @param newUrl
              * @param oldUrl
              */
 
-            self.setBg = function () {
+            self.setBackground = function () {
 
-                if (typeof bgImage.curBg !== "string" || bgImage.curBg.length === 0) {
+                if (typeof backgroundImage.currentBackground !== "string" || backgroundImage.currentBackground.length === 0) {
                     return;
                 }
 
-                $scope.bgs.push({
-                    url : bgImage.curBg,
-                    state : "preloading"
+                $scope.backgrounds.push({
+                    url: backgroundImage.currentBackground,
+                    state: "preloading"
                 });
 
-                var curBgNum = $scope.bgs.length - 1;
+                var currentBackgroundNum = $scope.backgrounds.length - 1;
 
                 var preload = new Image();
-                preload.src = bgImage.curBg;
+                preload.src = backgroundImage.currentBackground;
 
-                    if (preload.complete) {
-                        self.bgLoaded(curBgNum);
-                    } else {
-                        preload.addEventListener("load", function (e) {
-                            self.bgLoaded(curBgNum);
-                            this.remove();
-                        });
-                        preload.addEventListener("error", function() {
-                            console.warn("Error loading background image", this);
-                            this.remove();
-                        });
-                    }
+                if (preload.complete) {
+                    self.backgroundLoaded(currentBackgroundNum);
+                } else {
+                    preload.addEventListener("load", function (e) {
+                        self.backgroundLoaded(currentBackgroundNum);
+                        // remove the preloaded image element to prevent memory leak
+                        this.remove();
+                    });
+                    preload.addEventListener("error", function () {
+                        console.warn("Error loading background image", this);
+                        this.remove();
+                    });
+                }
 
             };
 
             /**
-             * Updates the src of the front image and sets the state to ready, which
-             * switches the opacity to 1. Called when the preloading is done.
-             * @param newUrl
+             * updates the state of the newly created background so that the dom element fades in
              */
-            self.bgLoaded = function (bgNum) {
-                setTimeout(function () {
-                    $scope.bgs[bgNum].state = "fadeIn";
+            self.backgroundLoaded = function (backgroundNum) {
+                // short timeout to ensure new background is loaded before start to fade in
+                $timeout(function () {
+                    $scope.backgrounds[backgroundNum].state = "fadeIn";
                 }, 50);
-                setTimeout(function () {
 
+                // timout to change do done after finished fading in.
+                // Fade-in is done with css, and it seems easier to use timeout than using an event for the transition complete.
+                // Need to make sure the timeout duration is longer than the transition duration
+                $timeout(function () {
                     // because this is a timeout, it won't the state change won't be picked up by the bound template
                     // but it doesn't matter. This is just for checking what can be removed.
-                    // timeout duration must be longer than the css fade in transition.
-                    $scope.bgs[bgNum].state = "done";
-                    self.cleanupBgs();
+                    $scope.backgrounds[backgroundNum].state = "done";
+                    self.cleanup();
                 }, 1000);
             };
 
 
             /**
-             * Removes all bgs from the list that are before (lower index) any with state 'done'
+             * Removes all backgrounds from the list that are before (lower index) any with state 'done'
              */
-            self.cleanupBgs = function () {
+            self.cleanup = function () {
 
                 var discard = false;
-                for (var i = $scope.bgs.length - 1; i >= 0; i--) {
+                for (var i = $scope.backgrounds.length - 1; i >= 0; i--) {
 
                     if (discard) {
-                        $scope.bgs[i].state = "finished";
-                    } else if ($scope.bgs[i].state === "done") {
+                        $scope.backgrounds[i].state = "finished";
+                    } else if ($scope.backgrounds[i].state === "done") {
                         discard = true;
                     }
                 }
             };
 
             $scope.$watch(function () {
-                return bgImage.curBg;
+                return backgroundImage.currentBackground;
             }, function (newVal, oldVal) {
-                self.setBg();
+                self.setBackground();
             });
 
         }]
